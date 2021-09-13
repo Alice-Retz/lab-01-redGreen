4c253494b61dfafe99fab2441c65c12b5162c143
Alice-Retz <alicegretz@gmail.com>
2021-09-13 17:23:01 -0500

    copyAndPush test written

---
+++ copy-and-push.test.js
@@ -0,0 +1,9 @@
+import copyAndPush from './copy-and-push';
+
+describe('copyAndPush', () => {
+    it('should add add 4 to the end of the array', () => {
+        const numbers = [1, 2, 3];
+        const num = copyAndPush(numbers, 4);
+        expect(num).toEqual([1, 2, 3, 4]);
+    });
+});
