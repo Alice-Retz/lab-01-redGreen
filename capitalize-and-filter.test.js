3de1bdeea3bbb0dbd0fad27757b034d9c255b3bd
Alice-Retz <alicegretz@gmail.com>
2021-09-13 19:17:46 -0500

    capitalizeAndFilter with loops test passing

--- capitalize-and-filter.test.js
+++ capitalize-and-filter.test.js
@@ -2,7 +2,7 @@ import capitalizeAndFilter from './capitalize-and-filter.js';
 
 -describe('capitalizeAndFilter', () => {
     it('capitalizes strings and removes those that start with f', () => {
-        const animals = ['jerboa', 'finch', 'Frog', 'okapi'];
+        const animals = ['jerboa', 'finch', 'Flamingo', 'okapi'];
         const newArr = capitalizeAndFilter(animals);
         expect(newArr).toEqual(['Jerboa', 'Okapi']);
     });
